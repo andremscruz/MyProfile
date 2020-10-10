8568416cf5b4624957ba3bbcc1a97b0772cd08a6
Andre Cruz <andremscruz10@hotmail.com>
2020-10-10 00:10:27 -0400

    commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,11 @@
+/*Andre Mariano Sousa Cruz - 301097077 - 10/09/2020 */
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
