8cc79b3071bc73d95627658fdf6ba9ec031b3609
Andre Cruz <andremscruz10@hotmail.com>
2020-10-23 01:15:42 -0400

    fixed delete button in contact page

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -4,6 +4,20 @@
     function Start()
     {
         console.log("App Started...");
+
+
+        let deleteButtons = document.querySelectorAll('.btn-outline-danger')
+
+        for(button of deleteButtons)
+        {
+            button.addEventListener('click', (event)=>{
+                if(!confirm("Are you sure?"))
+                {
+                    event.preventDefault();
+                    window.location.assign('/contact-list');
+                }
+            });
+        }
     }
 
     window.addEventListener("load", Start);
